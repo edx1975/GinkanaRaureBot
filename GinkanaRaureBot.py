3b63f7008175e33c4e8346605799d64e99b163a3
edx1975 <137057413+edx1975@users.noreply.github.com>
2025-09-23 01:05:43 +0200

    Add files via upload

--- GinkanaRaureBot.py
+++ GinkanaRaureBot.py
@@ -12,7 +12,7 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-TELEGRAM_TOKEN = "TELEGRAM_TOKEN"
+TELEGRAM_TOKEN = os.getenv"TELEGRAM_TOKEN"
 TARGET_DATE = dt(2025, 9, 28, 11, 0, 0)
 
 fixed_message_id = None
